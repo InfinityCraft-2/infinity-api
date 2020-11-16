b10f3d5a63fd55bea50214c1e319db9844cceecb
bradybellini <bbellini@pm.me>
2020-11-16 01:51:32 -0800

    Changed openapi info for docs

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
+from fastapi.openapi.utils import get_openapi
 
 from starlette.middleware import Middleware
 from starlette.middleware.authentication import AuthenticationMiddleware
@@ -7,9 +8,7 @@ from starlette.middleware.authentication import AuthenticationMiddleware
 from routers import players
 from utils import auth
 
-middleware = [
-    Middleware(AuthenticationMiddleware, backend=auth.KeyAuth())
-]
+middleware = [Middleware(AuthenticationMiddleware, backend=auth.KeyAuth())]
 
 app = FastAPI(middleware=middleware)
 
@@ -19,14 +18,26 @@ async def create_pool():
     pass
 
 
-@app.get("/test")
-async def test():
-    return {"test": "failed"}
-
-
 @app.get("/")
 async def root():
     return RedirectResponse("/v1/docs")
+
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="InfinityCraft 2.∞ API",
+        version="0.1.0",
+        description="Add Description Later",
+        routes=app.routes,
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
+    }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
 
 app.include_router(
     players.router,
@@ -34,3 +45,6 @@ app.include_router(
     tags=["players"],
     responses={404: {"description": "Endpoint not found"}},
 )
+
+
+app.openapi = custom_openapi
