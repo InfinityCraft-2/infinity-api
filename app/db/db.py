3d0efa36d343fe9adbd8e3af9236d6767431db26
bradybellini <bbellini@pm.me>
2020-11-16 21:48:56 -0800

    Creating queries from getting and creating players

--- app/db/db.py
+++ app/db/db.py
@@ -1,14 +1,31 @@
 import asyncpg
-from secret import Secret
 
 
-class PostgresConn:
-    async def make_pool(self):
-        async with asyncpg.create_pool(dsn=Secret.db_conn) as pool:
+class Pool:
 
-            async with pool.aquire() as con:
-                await con.fetch(
-                    """
-                    SELECT * FROM players
-                """
-                )
+    def __init__(self, db_conn):
+        self.db_conn = db_conn
+
+    async def pool(self):
+        pool = await asyncpg.create_pool(dsn=self.db_conn, command_timeout=10)
+        return await pool
+
+    async def aquire(self):
+        return await self.pool.aquire()
+
+
+class PlayerDb:
+
+    def __init__(self, pool):
+        self.pool = pool
+
+    async def new():
+        pass
+
+    async def all(self):
+        conn = await self.pool.aquire()
+        all_players = await conn.fetch("SELECT * FROM players")
+        return all_players
+
+    async def search():
+        pass
